4de9ea0dd2958107e3d81c8367a64757c94ad37a
YRG999 <name@example.com>
2022-12-13 10:24:54 -0500

    Update comments

--- tunedTweet.py
+++ tunedTweet.py
@@ -11,20 +11,15 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # Uncomment 1 or 2 only, not both
 
 # - - - - -
-
 # 1. Uncomment to specify maximum tokens and temperature
 max_tokens = input("Max tokens:\n")
 temp = input("Temperature:\n")
-
 # Get prompt from user
 input_prompt = input("Start or style of tweet:\n")
-
 # - - - - -
-
 # # 2. Uncomment to for random maximum tokens and temperature
 # max_tokens = str(random.randrange(200))
 # temp = "0."+str(random.randrange(9))
-
 # - - - - -
 
 # print("Max tokens (0-200)= ",max_tokens,"| Temperature (0-1, decimal)= ",temp)
@@ -33,6 +28,8 @@ token_temp = ("Max tokens (0-200)= ",max_tokens,"| Temperature (0-1, decimal)= "
 response = openai.Completion.create(
     # engine="text-davinci-002",
     engine="babbage:ft-personal-2022-06-02-04-05-09",
+
+# Use `input_prompt` for option 1 and null prompt for option 2
 #    prompt="",
     prompt=input_prompt,
     max_tokens=int(max_tokens),
